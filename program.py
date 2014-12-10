f6fa8799c0b6fa5d68ad3c722433cf798b7b6030
Brobin <tobinjbrown@gmail.com>
2014-12-09 19:55:58 -0600

    made the program accept file names

--- program.py
+++ program.py
@@ -1,18 +1,23 @@
 from assembler import Assembler
 
-# Runs the assembler. Takes an input file (code.s),
-# calls the assembler to compile it, and outputes the
-# result to code.mif. If an exception is thrown in the
-# assembler, it is caught and output here.
+# Runs the assembler. First ir prompts the user for
+# input and output file names. The input file should
+# be .s or .asm. The output files should be .mif.
+# Then it runs the assembler. If an exception is
+# raised, it is caught and the error message is
+# printed.
 
 program = Assembler()
-filename = "code.s"
-file = open(filename)
-code = file.readlines()
+filename = input("\nEnter the input file name: ")
+file_out = input("Enter the output file name: ")
 try:
+	file = open(filename)
+	print("\nAssembling...")
+	code = file.readlines()
 	output = program.compile(code)
-	output_file = open("code.mif", 'w')
+	output_file = open(file_out, 'w')
 	for line in output:
 		output_file.write("%s\n" % line)
+	print("\nAssembly successful!")
 except Exception as e:
 	print(str(e))
